22fbef334bf8384d1e104bedced32ff654a3eeb5
Krishnabh Medhi <kmedhi@purdue.edu>
2022-04-23 19:19:49 -0400

    Glass icons

--- constants.js
+++ constants.js
@@ -1,2 +1,80 @@
 var unitsRegex = /^oz\.?|drops?|dash(es)?|barspns?|tb?sps?|grams?|pints?|cups?$/;
+
 var SUGGESTION_COUNT_LIMIT = 5;
+
+var GLASSWARE = [
+	{
+		name: "absinthe",
+		regex: /absinthe\s+glass/i
+	},
+	{
+		name: "champagne",
+		regex: /champagne\s+(glass|flute)/i
+	},
+	{
+		name: "collins",
+		regex: /collins/i
+	},
+	{
+		name: "cordial",
+		regex: /cordial\s+glass/i
+	},
+	{
+		name: "coupe",
+		regex: /coupe/i
+	},
+	{
+		name: "doubleoldfashioned",
+		regex: /double\s+old[\-\s]fashioned\s+glass/i
+	},
+	// Order matters for these old fashioneds, regex below would
+	// test true for anything that matches regex above.
+	{
+		name: "oldfashioned",
+		regex: /old[\-\s]fashioned\s+glass/i
+	},
+	{
+		name: "goblet",
+		regex: /goblet/i
+	},
+	{
+		name: "highball",
+		regex: /highball\s+glass/i
+	},
+	{
+		name: "hurricane",
+		regex: /hurricane\s+glass/i
+	},
+	{
+		name: "largesnifter",
+		regex: /large\s+snifter/i
+	},
+	{
+		name: "martini",
+		regex: /martini\s+glass/i
+	},
+	{
+		name: "pilsner",
+		regex: /pilsner/i
+	},
+	{
+		name: "pint",
+		regex: /pint\s+glass/i
+	},
+	{
+		name: "rocks",
+		regex: /rocks\s+glass/i
+	},
+	{
+		name: "snifter",
+		regex: /snifter/i
+	},
+	{
+		name: "toddy",
+		regex: /toddy\s+glass/i
+	},
+	{
+		name: "wine",
+		regex: /wine\s+glass/i
+	}
+];
--- search.js
+++ search.js
@@ -7,40 +7,14 @@ function submitOnEnterPressed(event) {
 	}
 }
 
-function getFormattedIngredient(ingredient) {
-	var ingredientContainer = document.createElement('div');
-	ingredientContainer.className = "ingredient";
-
-	var measure = document.createElement("span");
-	measure.className = "measure";
-	var unit = document.createElement("span");
-	unit.className = "unit";
-	var rest = document.createElement("span");
-	rest.className = "rest";
-
-	var regex = /^([\d\.\-]*)\s+(oz\.?|drops?|dash(es)?|barspns?|tb?sps?|grams?|pints?|cups?)?\s*(.*)/;
-	var matches = ingredient.match(regex);
-	if (matches && matches.length >= 5) {
-		measure.innerHTML = matches[1] || "";
-		unit.innerHTML = matches[2] || "";
-		rest.innerHTML = matches[4] || "";
-	} else {
-		rest.innerHTML = ingredient;
-	}
-
-	ingredientContainer.appendChild(measure);
-	ingredientContainer.appendChild(unit);
-	ingredientContainer.appendChild(rest);
-	return ingredientContainer;
-}
-
 function search() {
 	var matches = [...recipes];
 
 	var searchName = document.getElementById("recipeNameInput").value;
 	if (searchName) {
-		var recipeNameLookupRegex = new RegExp("\\b" + searchName + "\\b", "i");
-		matches = matches.filter((recipe) =>
+		var recipeNameLookupRegex = new RegExp("\\b" + searchName + "\\b", "ig");
+		console.log(recipeNameLookupRegex)
+		matches = matches.filter((recipe) => 
 			recipeNameLookupRegex.test(recipe.name)
 		);
 	}
@@ -48,7 +22,7 @@ function search() {
 	for (var i = 0; i < 5; i++) {
 		var searchIngredient = document.getElementById("ingredient-" + i).value;
 		if (searchIngredient) {
-			var ingredientLookupRegex = new RegExp("\\b" + searchIngredient + "\\b", "i");
+			var ingredientLookupRegex = new RegExp("\\b" + searchIngredient + "\\b", "ig");
 			matches = matches.filter((recipe) =>
 				recipe.ingredients.some((ingredient) =>
 					ingredientLookupRegex.test(ingredient)
@@ -66,22 +40,25 @@ function search() {
 		matches
 			.sort(() => Math.random() - 0.5)
 			.forEach((recipe) => {
-				var matchContainer = document.createElement('div');
-				matchContainer.className = "match";
-				matchContainer.innerHTML = "<div class='recipeTopRow'>"
-					+ "<div><h2 class='recipeTitle'>" + recipe.name.toUpperCase() + "</h2></div>"
-					+ "<img class='closeIcon modalCloseBtn' src='close-icon.svg'/>"
-					+ "</div>";
-				recipe.ingredients.forEach((ingredient) => {
-					var formattedIngredient = getFormattedIngredient(ingredient);
-					matchContainer.appendChild(formattedIngredient);
-				});
-				matchContainer.innerHTML += "<div class='procedure'>" + recipe.procedure + "</div>";
+				var matchContainer = generateRecipeCard(recipe);
 				matchContainer.onclick = modalizeRecipe(matchContainer);
 				resultContainer.appendChild(matchContainer);
 			});
 		resultsSummaryContainer.innerHTML = `Found ${matches.length} cocktails.`;
 	}
+
+	// The following logic uses a library to
+	// split text titles more intelligently.
+	//
+	// Instead of:
+	// | CARAWAY MY WAYWARD |
+	// |        SON         |
+	//
+	// the balanced break does:
+	// |    CARAWAY MY      |
+	// |    WAYWARD SON     |
+	var recipeTitleContainers = document.getElementsByClassName("recipeTitle");
+	balanceText(recipeTitleContainers);
 };
 
 submitBtn.onclick = search;
